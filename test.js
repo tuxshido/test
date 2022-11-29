25d7a93cceac80435d85125b771a1481dd0651ec
tuxshido <c.metman@e.email>
2022-11-29 15:18:31 +0900

    Add .fikarc for fika configuration

---
+++ test.js
@@ -0,0 +1,19 @@
+let be = 455;
+be = 577:
+be =  5 + 6; 
+out of the branch;
+basebranch;
+be +=5;
+be = well? 5: 6: 
+loking for conflict;
+Hohohoh c pa bien;
+are you here;
+coding in the basebranch;
+insane in the meme brain;
+all right, all right;
+for the beauty;
+black hole sun;
+stone temple pilots;
+daft poonk;
+mamamia !!;:wq
+et quoi bouffon;
